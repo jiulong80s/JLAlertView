8a63c359a439f16e0d9bc958263668a20c387ecb
jiulong80s <202060538@163.com>
2016-02-26 10:48:48 +0800

    添加 JLAlerview 调用实例

--- Test/Test/ViewController.m
+++ Test/Test/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "JLAlertView.h"
 @interface ViewController ()
 
 @end
@@ -25,7 +25,32 @@
 }
 
 - (IBAction)click:(UIButton *)sender {
+	JLAlertView *alert = nil;
+	switch (sender.tag) {
+		case 1:
+			alert = [[JLAlertView alloc] initWithTitle:@"提示1" detailText:@"测试一下 JLAlertview" customView:nil cancelButtonTitle:@"取消" otherButtonTitles:@[@"确定"]];
+				break;
+		case 2:
+			alert = [[JLAlertView alloc] initWithTitle:@"提示2" detailText:@"测试一下 JLAlertview\n换个行试试" customView:nil cancelButtonTitle:@"取消" otherButtonTitles:@[@"确定"]];
+			break;
+		case 3:
+			alert = [[JLAlertView alloc] initWithTitle:@"提示3" detailText:@"测试一下 JLAlertview，多个按钮试试" customView:nil cancelButtonTitle:@"取消" otherButtonTitles:@[@" button1",@" button2"]];
+			break;
+		case 4:
+		{
+			UIView *view = [[UIView alloc] initWithFrame:CGRectMake(10, 0, 180, 50)];
+			view.backgroundColor = [UIColor orangeColor];
+			alert = [[JLAlertView alloc] initWithTitle:@"提示4" detailText:@"测试一下 JLAlertview,添加自定义视图" customView:view cancelButtonTitle:@"取消" otherButtonTitles:@[@"确定"]];
+		}
+			break;
+			
+	  default:
+				break;
+	}
 	
+	[alert showWithBlock:^(NSInteger index) {
+		NSLog(@"click button index:%ld",index);
+	}];
 	
 }
 @end
